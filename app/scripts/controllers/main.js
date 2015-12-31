f0fd8a209494d99e63a29b03e04a568c37a34c1f
Ignacio Lupi <nacholupi@gmail.com>
2015-12-30 22:11:47 -0300

    Sortable 18

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,4 +10,11 @@
 angular.module('angularTestApp')
   .controller('MainCtrl', function ($scope) {
     $scope.tareas = ['Item1', 'Item2', 'Item3'];
+    $scope.addTarea = function() {
+    	$scope.tareas.push($scope.tarea);
+    	$scope.tarea = '';
+    };
+    $scope.eliminarTarea = function(index){
+    	$scope.tareas.splice(index, 1);
+    };
   });
